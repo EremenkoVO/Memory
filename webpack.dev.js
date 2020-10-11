10b60d862a1b5066fff9ee481350684c5d36487d
Vadim Eremenko <eremenko.v.o@yandex.ru>
2020-10-11 17:47:59 +0300

    update build script

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "js/index.js",
+    filename: "index.js",
   },
   devServer: {
     contentBase: "./dist",
